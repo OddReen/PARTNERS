5aa215e21cecec07c840ee87948e58de721cac98
BernardoSoaresPT <beni.c.soares.2002@gmail.com>
2023-09-28 16:04:53 +0100

    Multiplayer Test Begin

---
+++ Assets/MultiplayerTest/Scripts/MultyplayerUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultyplayerUI : MonoBehaviour
+{
+    [SerializeField] private Button hostBT;
+    [SerializeField] private Button serverBT;
+    [SerializeField] private Button clientBT;
+    private void Awake()
+    {
+        hostBT.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        serverBT.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
+        clientBT.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
